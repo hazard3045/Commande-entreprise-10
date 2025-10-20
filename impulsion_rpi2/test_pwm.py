629e13da40b49a697dbef368b4d7ff64ea566650
Thomas Bruyère <thomas.bruyere@imt-atlantique.net>
2025-10-20 09:41:54 +0200

    Add clock sync
    Refactor PWM setup and loop for GPIO control.

--- impulsion_rpi2/test_pwm.py
+++ impulsion_rpi2/test_pwm.py
@@ -5,11 +5,23 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(33,GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(32,GPIO.OUT, initial = GPIO.LOW)
 
-for i in range (100):
-  GPIO.output(33,GPIO.HIGH)
-  time.sleep(100)
+p = GPIO.PWM(32,1)
+p.start(10)
+
+i=100
+
+while(1):
+  if (i >0):
+    GPIO.output(33,GPIO.HIGH)
+    time.sleep(100)
+    i -=1
+  else:
+    GPIO.output(33,GPIO.LOW)
   GPIO.output(33,GPIO.LOW)
   time.sleep(900)
+
+
 
 GPIO.cleanup()
