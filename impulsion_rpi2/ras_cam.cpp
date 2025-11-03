d5d90363b685adcc297116121ce1727f046996eb
Thomas Bruyère <thomas.bruyere@imt-atlantique.net>
2025-11-03 11:46:13 +0100

    Improve error handling and output in ras_cam.cpp

--- impulsion_rpi2/ras_cam.cpp
+++ impulsion_rpi2/ras_cam.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <pigpio.h>
 #include <unistd.h>
+#include <iomanip> 
+
+using namespace std;
 int main(){
-    if(gpioInitialise() < 0){}
+    if(gpioInitialise() < 0){cout<<"can't connect to pigpiodaemon"<<endl;}
     else{
         const int pin = 13;
         gpioSetMode(pin , PI_INPUT);
         int level = 0;
         int new_level;
-        int i=0;
+        int nbImpulsions = 0;
         while(1){
              new_level= gpioRead(pin);
+             cout<<gpioRead(pin)<<endl;
              if(new_level != level){
-                printf("GPIO33 is level %d", new_level);
+                cout<<"GPIO33 is level %d"<<new_level<<endl;
                 level = new_level;
-                i=i+1;
-                printf("total reçu %d " , i);
+                nbImpulsions=nbImpulsions+1;
+                cout<<"total reçu"<<nbImpulsions<<endl;
              }
             
         }    
     }
+    gpioTerminate();
     return 0 ;
 }
