5b7e32f0752ee1801b7400096952186ae622b1f7
meniche tinhinen <meniche.tinhinen@gmail.com>
2025-11-24 10:35:39 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <pigpio.h>
+#include <unistd.h>
+#include <iomanip> 
+using namespace std;
+int nbImpulsions; // compteur d'impulsion 
+int clk_externe; // la clock externe donné par le GPS 
+bool impulsion; // True si il y a eu une impulsion False sinon 
+ 
+// Fonction 
+
+void prendre_photo(){
+    // une fonction qui prend la photo et return la photo
+}
+
+int*int*int tag_photo(){
+    // tager la photo avec (nbImpulsion,clk_externe,clk_interne)
+    return(1,1,1)
+}
+
+void interrupt_impuls(){
+    
+}
+
+void interrupt_clk{
+
+}
+
+bool stock_photo(){
+//prend une photo  en paramétre et la stock sur la carte SD renvoie True quand c'est terminé 
+return True 
+}
+
+void compresser_photo(){
+    // prend une photo en paramétre la compresse et return la photo
+}
+
+
+//main 
+int main(){
+    return 1
+}
