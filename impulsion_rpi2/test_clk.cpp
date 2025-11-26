52c556cc9f18d6c8241ac22c9a29366e3f010ff4
Thomas Bruyère <thomas.bruyere@imt-atlantique.net>
2025-11-26 19:54:38 +0100

    Refactor output messages and loop structure

--- impulsion_rpi2/test_clk.cpp
+++ impulsion_rpi2/test_clk.cpp
@@ -3,29 +3,37 @@
 #include <thread>
 #include <iostream>
 
-using namespace std;
-
 uint32_t clk_rising = 0;
 uint32_t clk_falling = 0;
 
-
 void interrupt_clk_rising(int gpio, int level, uint32_t tick){
     clk_rising = tick;
-    cout << "j'ai reçu un front montant" << endl;
+    std::cout << "Front montant reçu !" << std::endl;
 }
 
 void interrupt_clk_falling(int gpio, int level, uint32_t tick){
-    clk_falling =tick;
-    cout << "Clock Period: " << (clk_falling - clk_rising) << " microseconds" << endl;
+    clk_falling = tick;
+    std::cout << "Periodo : " << (clk_falling - clk_rising) << " µs" << std::endl;
 }
 
 int main() {
+
     if (gpioInitialise() < 0) {
         std::cerr << "Pigpio initialization failed!" << std::endl;
         return 1;
     }
-    cout << "je lance le programme" << endl;
+
+    std::cout << "Programme démarré" << std::endl;
+
+    gpioSetMode(13, PI_INPUT);
+    gpioSetPullUpDown(13, PI_PUD_OFF);  // ou PI_PUD_UP selon ton signal
+
     gpioSetISRFunc(13, RISING_EDGE, 0, interrupt_clk_rising);
     gpioSetISRFunc(13, FALLING_EDGE, 0, interrupt_clk_falling);
-    while(1){}
+
+    while (true)
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    gpioTerminate();
+    return 0;
 }
