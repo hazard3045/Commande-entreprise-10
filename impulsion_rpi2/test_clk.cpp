d4a3452536c3d6df59840b63062d6f38348579cd
Thomas Bruyère <thomas.bruyere@imt-atlantique.net>
2025-11-26 20:41:58 +0100

    Refactor GPIO usage to use variable for pin number

--- impulsion_rpi2/test_clk.cpp
+++ impulsion_rpi2/test_clk.cpp
@@ -12,6 +12,8 @@ void falling_callback(int gpio, int level, uint32_t tick) {
 }
 
 int main() {
+    int pi = 17;
+    
     // Initialisation pigpio
     if (gpioInitialise() < 0) {
         std::cerr << "Erreur : Pigpio initialization failed!" << std::endl;
@@ -20,17 +22,17 @@ int main() {
     std::cout << "Programme démarré, test ISR sur GPIO 13" << std::endl;
 
     // Configurer GPIO 13 comme entrée avec pull-down pour signal flottant
-    gpioSetMode(13, PI_INPUT);
-    gpioSetPullUpDown(13, PI_PUD_DOWN); // ou PI_PUD_UP selon ton signal
+    gpioSetMode(pi, PI_INPUT);
+    gpioSetPullUpDown(pi, PI_PUD_DOWN); // ou PI_PUD_UP selon ton signal
 
     // Installer les callbacks ISR
-    gpioSetISRFunc(13, RISING_EDGE, 0, rising_callback);
-    gpioSetISRFunc(13, FALLING_EDGE, 0, falling_callback);
+    gpioSetISRFunc(pi, RISING_EDGE, 0, rising_callback);
+    gpioSetISRFunc(pi, FALLING_EDGE, 0, falling_callback);
 
     // Boucle principale : ne pas bloquer le thread ISR
     while (true) {
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
-        std::cout << gpioRead(13) << std::endl;
+        std::cout << gpioRead(pi) << std::endl;
     }
 
     gpioTerminate(); // jamais atteint ici
