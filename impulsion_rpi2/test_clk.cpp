1bf1540024e420804929772500b3df82332b4c26
Thomas Bruyère <thomas.bruyere@imt-atlantique.net>
2025-11-27 11:16:02 +0100

    Update GPIO pin from 13 to 17 and clean up code

--- impulsion_rpi2/test_clk.cpp
+++ impulsion_rpi2/test_clk.cpp
@@ -3,27 +3,31 @@
 #include <chrono>
 #include <thread>
 
+bool bascule;
+uint32_t global_tick;
+
 void rising_callback(int gpio, int level, uint32_t tick) {
-    std::cout << "Front montant détecté à tick=" << tick << std::endl;
+    bascule = true;
+    global_tick = tick;
 }
 
-void falling_callback(int gpio, int level, uint32_t tick) {
-    std::cout << "Front descendant détecté à tick=" << tick << std::endl;
-}
+void falling_callback(int gpio, int level, uint32_t tick) {}
 
 int main() {
     int pi = 17;
+
+    bascule = false;
     
     // Initialisation pigpio
     if (gpioInitialise() < 0) {
         std::cerr << "Erreur : Pigpio initialization failed!" << std::endl;
         return 1;
     }
-    std::cout << "Programme démarré, test ISR sur GPIO 13" << std::endl;
+    std::cout << "Programme démarré, test ISR sur GPIO 17" << std::endl;
 
-    // Configurer GPIO 13 comme entrée avec pull-down pour signal flottant
+    // Configurer GPIO 17 comme entrée avec pull-down pour signal flottant
     gpioSetMode(pi, PI_INPUT);
-    gpioSetPullUpDown(pi, PI_PUD_DOWN); // ou PI_PUD_UP selon ton signal
+    gpioSetPullUpDown(pi, PI_PUD_DOWN); 
 
     // Installer les callbacks ISR
     gpioSetISRFunc(pi, RISING_EDGE, 0, rising_callback);
@@ -31,6 +35,10 @@ int main() {
 
     // Boucle principale : ne pas bloquer le thread ISR
     while (true) {
+        if (bascule == true){
+            bascule = false;
+            std::cout << "rise happened at" << global_tick << std::endl;
+        }
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
         std::cout << gpioRead(pi) << std::endl;
     }
