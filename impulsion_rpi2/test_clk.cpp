f70b976b3b89906b636bacf4602cda5c771b7931
Thomas Bruyère <thomas.bruyere@imt-atlantique.net>
2025-11-28 18:10:07 +0100

    Refactor test_clk.cpp for clarity and functionality

--- impulsion_rpi2/test_clk.cpp
+++ impulsion_rpi2/test_clk.cpp
@@ -2,46 +2,38 @@
 #include <iostream>
 #include <thread>
 
-bool bascule ;
-uint32_t global_tick ;
+bool bascule = false;
+uint32_t global_tick = 0;
 
 void rising_callback(int gpio, int level, uint32_t tick) {
     bascule = true;
     global_tick = tick;
 }
 
-//void falling_callback(int gpio, int level, uint32_t tick) {}
-
 int main() {
-    int pi = 17;
 
-    bascule = false;
-    
-    // Initialisation pigpio
     if (gpioInitialise() < 0) {
-        std::cerr << "Erreur : Pigpio initialization failed!" << std::endl;
+        std::cerr << "Erreur : Pigpio init failed\n";
         return 1;
     }
-    std::cout << "Programme démarré, test ISR sur GPIO 17" << std::endl;
 
-    // Configurer GPIO 17 comme entrée avec pull-down pour signal flottant
-    gpioSetMode(pi, PI_INPUT);
-    gpioSetPullUpDown(pi, PI_PUD_DOWN); 
+    int gpio = 17;
+    std::cout << "Programme démarré, test ISR sur GPIO 17\n";
 
-    // Installer les callbacks ISR
-    gpioSetISRFunc(pi, RISING_EDGE, 0, rising_callback);
-    //gpioSetISRFunc(pi, FALLING_EDGE, 0, falling_callback);
+    gpioSetMode(gpio, PI_INPUT);
+    gpioSetPullUpDown(gpio, PI_PUD_DOWN);
 
-    // Boucle principale : ne pas bloquer le thread ISR
+    // ALERT func = en mode pollé ultra-rapide, déclenché à chaque changement
+    gpioSetAlertFunc(gpio, rising_callback);
+
     while (true) {
-        if (bascule == true){
+        if (bascule) {
             bascule = false;
-            std::cout << "rise happened at" << global_tick << std::endl;
+            std::cout << "Front détecté, tick = " << global_tick << "\n";
         }
-       // std::this_thread::sleep_for(std::chrono::milliseconds(200));
-       // std::cout << gpioRead(pi) << std::endl;
+        gpioDelay(1000);  // 1ms
     }
 
-    gpioTerminate(); // jamais atteint ici
+    gpioTerminate();
     return 0;
 }
