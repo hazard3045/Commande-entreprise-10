1f8630e5d6519c5d428bc79815634d5ffc929e5f
thomas <thomas.bruyere@imt-atlantique.net>
2025-12-07 11:01:24 +0100

    fusion du code

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include <pigpio.h>
 #include <unistd.h>
 #include <iomanip> 
+
 using namespace std;
-int nbImpulsions; // compteur d'impulsion 
-int clk_externe; // la clock externe donné par le GPS 
-bool impulsion; // True si il y a eu une impulsion False sinon 
+int nbImpulsions = 0 ; // compteur d'impulsion 
+int clk_externe = 0; // la clock externe donné par le GPS 
+bool impulsion = false; // True si il y a eu une impulsion False sinon 
+int gpio_imp = 17; // GPIO pour les impulsions
+int gpio_clk = 27; // GPIO pour la clock externe
+
  
 // Fonction 
 
@@ -13,10 +17,10 @@ string prendre_photo(){
     // Génère un nom de fichier avec le format : photo_nbImpulsions_clk_externe_clk_interne
     string nomFichier = "photo_" + to_string(nbImpulsions) + "_" + 
                         to_string(clk_externe) + "_" + 
-                        to_string(clk_interne) + ".jpg";
+                        to_string(clk_interne) + ".dng";
     
     // Construit la commande libcamera-jpeg
-    string commande = "libcamera-jpeg -o " + nomFichier + " --nopreview -t 1";
+    string commande = "libcamera-raw -o " + nomFichier + " --nopreview -t 1";
     
     // Exécute la commande
     int resultat = system(commande.c_str());
@@ -31,16 +35,20 @@ string prendre_photo(){
 }
 
 int*int*int tag_photo(){
-    // tager la photo avec (nbImpulsion,clk_externe,clk_interne)
-    return(1,1,1)
+    return (nbImpulsions, clk_externe, gpioTick());
 }
 
-void interrupt_impuls(){
-    
+void rising_callback_clk(int gpio, int level, uint32_t tick) {
+    if (level == 1){
+        clk_externe += 1;
+    }
 }
 
-void interrupt_clk{
-
+void rising_callback_impul(int gpio, int level, uint32_t tick) {
+    if (level == 1){
+        impulsion = true;
+        nbImpulsions += 1;
+    }
 }
 
 bool stock_photo(){
@@ -55,5 +63,30 @@ void compresser_photo(){
 
 //main 
 int main(){
-    return 1
+
+    if (gpioInitialise() < 0) {
+        std::cerr << "Erreur : Pigpio init failed\n";
+        return 1;
+    }
+
+    std::cout << "Programme démarré, test ISR sur GPIO 17\n";
+
+    gpioSetMode(gpio_imp, PI_INPUT);
+    gpioSetMode(gpio_clk, PI_INPUT);
+    gpioSetPullUpDown(gpio_imp, PI_PUD_DOWN);
+    gpioSetPullUpDown(gpio_clk, PI_PUD_DOWN);
+
+    // ALERT func = en mode pollé ultra-rapide, déclenché à chaque changement
+    gpioSetAlertFunc(gpio_imp, rising_callback_impulsion);
+    gpioSetAlertFunc(gpio_clk, rising_callback_clk);
+
+
+    while (true){
+        if (impulsion == true){
+            impulsion = false;
+            string photo = prendre_photo();
+            stock_photo(photo);
+        }
+        usleep(1000); // Attendre 1 ms pour éviter une boucle trop rapide
+    }
 }
